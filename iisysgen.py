1834e710f014a4d61c9725317116cc3bb1ba13fa
Colin Hogben <colin@infinnovation.co.uk>
2020-01-08 09:31:48 +0000

    iisysgen: Merge multiple configs and individual variables
    Allow e.g. proxy to be set outside configuration file.

--- iisysgen.py
+++ iisysgen.py
@@ -135,12 +135,31 @@ def main():
     ap = argparse.ArgumentParser()
     #ap.add_argument('--chroot',
     #                help='Use chroot for building')
+    def vardef(arg):
+        """Map e.g. 'user.name=Fred' to {'user':{'name':'Fred'}}"""
+        n, _, v = arg.partition('=')
+        try:
+            v = int(v,0)
+        except ValueError:
+            try:
+                v = float(v)
+            except ValueError:
+                pass
+        def path2dict(path, val):
+            step, sep, rest = path.partition('.')
+            if sep:
+                return {step: path2dict(rest, val)}
+            return {path: val}
+        return path2dict(n, v)
+    #
     sub = ap.add_subparsers(dest='method', metavar='METHOD')
     #
     m_build = sub.add_parser('build',
                              help='Build installation')
-    m_build.add_argument('-c','--config',
+    m_build.add_argument('-c','--config', action='append', default=[],
                          help='Configuration in JSON or YAML')
+    m_build.add_argument('-v','--var', type=vardef, action='append', default=[],
+                         help='Definition var=value to add to configuration')
     m_build.add_argument('-x','--export', action='store_true',
                          help='Export built filetree')
     m_build.add_argument('builder',
@@ -159,21 +178,26 @@ def main():
     method = args.method
     if method == 'build':
         cfg = {}
-        if args.config:
-            config = args.config
+        for config in args.config:
             if config.endswith('.json'):
                 import json
                 with open(config) as f:
-                    cfg = json.load(f)
+                    fcfg = json.load(f)
+                if fcfg is not None:
+                    cfg_merge(cfg, fcfg)
             elif config.endswith('.yaml'):
                 try:
                     import yaml
                 except ImportError:
                     raise RuntimeError('YAML not supported')
                 with open(config) as f:
-                    cfg = yaml.load(f)
+                    fcfg = yaml.load(f)
+                if fcfg is not None:
+                    cfg_merge(cfg, fcfg)
             else:
                 raise ValueError('Unknown config file extension')
+        for vcfg in args.var:
+            cfg_merge(cfg, vcfg)
         builder = find_builder(args.builder)
         gen = DockerGen(args.builder)
         builder.build(gen, cfg)
@@ -189,6 +213,33 @@ def find_builder(name):
     from importlib import import_module
     mod = import_module(name)
     return mod
+
+def cfg_merge(cfg, extra, path=[]):
+    """Merge configuration data"""
+    for name, value in extra.items():
+        if name in cfg:
+            branch = cfg[name]
+            if isinstance(branch, dict):
+                if not isinstance(value, dict):
+                    raise ValueError('Cannot merge dict with %s at %s' %
+                                     (type(value), '/'.join(path)))
+                cfg_merge(branch, value, path+[name])
+            elif isinstance(branch, list):
+                if not isinstance(value, list):
+                    raise ValueError('Cannot extend list with %s at %s' %
+                                     (type(value), '/'.join(path)))
+                branch.extend(value)
+            elif isinstance(value, dict):
+                raise ValueError('Cannot merge %s with dict at %s' %
+                                 (type(branch), '/'.join(path)))
+            elif isinstance(value, list):
+                raise ValueError('Cannot merge %s with list at %s' %
+                                 (type(branch), '/'.join(path)))
+            else:
+                # Replace existing scalar
+                cfg[name] = value
+        else:
+            cfg[name] = value
 
 if __name__=='__main__':
     main()
