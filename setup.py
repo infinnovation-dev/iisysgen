8995792b96b11ae6ea07f0475098544f8d5f2854
Colin Hogben <colin@infinnovation.co.uk>
2020-01-16 12:32:33 +0000

    Add setup.py and license for distribution

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+setup(name='iisysgen',
+      version='0.1',
+      description="Tools for generating GNU/Linux system images",
+      author="Colin Hogben",
+      author_email="colin@infinnovation.co.uk",
+      project_urls={
+          "Source Code": "https://github.com/infinnovation-dev/iisysgen.git",
+      },
+      license='MIT',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Topic :: System :: Installation/Setup',
+          'Operating System :: POSIX :: Linux'
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+      ],
+      py_modules=['iisysgen'],
+      entry_points={
+          'console_scripts': [
+              'iisysgen = iisysgen:main',
+          ],
+      },
+)
